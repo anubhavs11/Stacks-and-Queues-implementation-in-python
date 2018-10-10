fc9ab45bf5b96b8a560f9b4ae7b4b9f61bc2e7c4
Anubhav Singh <35691558+anubhavs11@users.noreply.github.com>
2018-10-11 01:19:47 +0530

    Create All_Pair_Shortest_Path_(_Floyd_Warshall_).cpp

---
+++ Dynamic Programming/All_Pair_Shortest_Path_(_Floyd_Warshall_).cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int a[n][n];
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+			 cin>>a[i][j];
+
+	for (int k = 0; k < n; ++k){
+		for (int i = 0; i < n; ++i){
+			for (int j = 0; j < n; ++j){
+				a[i][j] = min(a[i][j],a[i][k]+a[k][j]);
+			}
+		}
+	}
+
+	for (int i = 0; i < n; ++i){
+		for (int j = 0; j < n; ++j)
+			cout<<a[i][j]<<" ";
+		cout<<endl;
+	}
+}
