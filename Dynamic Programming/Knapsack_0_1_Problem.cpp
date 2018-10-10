32b1bb48ea47fc425e2214300cda8de880b76fc4
Anubhav Singh <35691558+anubhavs11@users.noreply.github.com>
2018-10-11 00:57:22 +0530

    Update Knapsack_0_1_Problem.cpp

--- Dynamic Programming/Knapsack_0_1_Problem.cpp
+++ Dynamic Programming/Knapsack_0_1_Problem.cpp
@@ -1,42 +1,42 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-	
-	int t;
-	cin>>t;
-	while(t>0){
-	    t--;
-	    int n;
-	    cin>>n;
-	    int W;
-	    cin>>W;
-	    int val[n+1],wt[n+1];
-	    for(int i=0;i<n;i++)
-	        cin>>val[i];
-	    for(int i=0;i<n;i++)
-	        cin>>wt[i];
-	        
-	    int k[n+1][W+1];
+void Knapsack(int wt[],int val[],int n,int W){
+	int k[n+1][W+1];
 
-        for (int i = 0; i < n+1; ++i){
-        	for (int w = 0; w < W+1; ++w)
-        	{
-        		if(i==0 || w==0)
-        			k[i][w] = 0;
-        
-        		else if(w >= wt[i-1]){
-        			if(w-wt[i-1]>=0)
-        				k[i][w] = max(k[i-1][w],val[i-1]+k[i-1][w-wt[i-1]]);
-        			else
-        				k[i][w] = k[i-1][w];
-        		}
-        		else{
-        			k[i][w] = k[i-1][w];
-        		}
-        	}
-        }
-        cout<<k[n][W]<<endl;
+	for (int i = 0; i < n+1; ++i){
+		for (int w = 0; w < W+1; ++w)
+		{
+			if(i==0 || w==0)
+				k[i][w] = 0;
+
+			else if(w >= wt[i-1]){
+				if(w-wt[i-1]>=0)
+					k[i][w] = max(k[i-1][w],val[i-1]+k[i-1][w-wt[i-1]]);
+				else
+					k[i][w] = k[i-1][w];
+			}
+			else{
+				k[i][w] = k[i-1][w];
+			}
+		}
 	}
+	cout<<k[n][W]<<endl;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	int W;
+	cin>>W;
+	int wt[n+1],val[n+1];
+
+	for (int i = 1; i < n+1; ++i)
+	{
+		cin>>wt[i];
+		cin>>val[i];
+	}
+	Knapsack(wt,val,n,W);
 	return 0;
 }
