c1d426cf11b3ea5387e3d086f18dbd1f9624653a
Anubhav Singh <35691558+anubhavs11@users.noreply.github.com>
2019-02-13 02:11:42 +0530

    Update LevelOrderTraversal.cpp

--- Tree-implementation-cpp/LevelOrderTraversal.cpp
+++ Tree-implementation-cpp/LevelOrderTraversal.cpp
@@ -81,6 +81,36 @@ public:
     void levelOrderTraversal(){
         levelOrder(root);
     }
+    int getHeight(Node *node){
+    	if(!node)
+    		return 0;
+    	return 1+max(getHeight(node->left),getHeight(node->right));
+    }
+    int traverse(Node *node,int h,int sum){
+    	if(node!=NULL){
+    		
+    		sum=traverse(node->left,h,sum);
+    		sum=traverse(node->right,h,sum);
+
+    		if(getHeight(node)==h){
+    			sum+=node->data;
+    		}
+    	}
+    	return sum;
+    }
+    void ODD_EVEN_LEVEL_DIFFERENCE(){
+        int height = getHeight(root);
+        int sum_odd = 0;
+        int sum_even=0;
+        for (int i = 1; i <=height; ++i){
+        	if(i%2!=0)
+        		sum_odd+=traverse(root,i,0);
+        	else
+        		sum_even+=traverse(root,i,0);
+        }
+        cout<<"Difference : "<<abs(sum_odd-sum_even);
+    }
+
 };
 
 int main(){
@@ -93,4 +123,10 @@ int main(){
     ob.display();
     cout<<"\nLevel Order Traversal:";
     ob.levelOrderTraversal();
+    
+    Tree ob2;
+    for (int i = 1; i < 11; ++i)    {
+    	ob2.insert(i);
+    }
+    ob2.ODD_EVEN_LEVEL_DIFFERENCE();
 }
